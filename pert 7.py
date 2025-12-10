033d35c6560e3ae882a61b3ab448904fd0430ab2
nasywafaradiba <nasywa.124120055@student.itera.ac.id>
2025-12-10 14:17:13 +0700

    Add files via upload

---
+++ pert 7.py
@@ -0,0 +1,12 @@
+# %%
+import numpy as np
+my_list = [10, 20, 30, 40, 50]
+my_array = np. array(my_list)
+print (my_array)
+
+hasil_kali = my_array*2
+print (hasil_kali)
+
+hasil_tambah = my_array + 5
+print (hasil_tambah)
+
